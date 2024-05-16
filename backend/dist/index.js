c1483f433419af158709df2cd7544356ecd709fe
Khyati Sharma <khyatisharma284@gmail.com>
2024-05-16 14:22:02 +0530

    Initial commit

---
+++ backend/dist/index.js
@@ -0,0 +1,8 @@
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+connectToDatabase()
+    .then(() => {
+    app.listen(process.env.PORT, () => console.log("Server Open & Connected To Database!"));
+})
+    .catch((err) => console.log(err));
+//# sourceMappingURL=index.js.map
